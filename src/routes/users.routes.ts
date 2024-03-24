618e400bccfefbf8ca487420dd91f96623a3ac85
Agleylson Melo <agleylsonmelo@outlook.com>
2024-03-24 10:36:04 -0300

    create user

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,7 +1,34 @@
+import { randomUUID } from "node:crypto";
 import { FastifyInstance } from "fastify";
+import { z } from "zod";
+
+import { knex } from "../database";
 
 export async function usersRoutes(app: FastifyInstance) {
-  app.get("/", async (request, reply) => {
-    return reply.send("Hello World!");
+  app.post("/", async (request, reply) => {
+    const createUserBodySchema = z.object({
+      name: z.string(),
+      email: z.string().email(),
+    });
+
+    const { name, email } = createUserBodySchema.parse(request.body);
+
+    let sessionId = request.cookies.sessionId;
+
+    if (!sessionId) {
+      sessionId = randomUUID()
+
+      reply.cookie("sessionId", sessionId, {
+        path: "/",
+        maxAge: 60 * 60 * 24 * 7, // 7 days
+      })
+    }
+
+    await knex("users").insert({
+      id: randomUUID(),
+      name,
+      email,
+      session_id: sessionId,
+    });
   });
 }
